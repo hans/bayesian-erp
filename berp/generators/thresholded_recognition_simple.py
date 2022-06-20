f97ec9a09f882441662b979ce17e41d5917721a6
Jon Gauthier <jon@gauthiers.net>
2022-06-20 15:40:31 +0000

    continue level1 refactor into response model

--- berp/generators/thresholded_recognition_simple.py
+++ berp/generators/thresholded_recognition_simple.py
@@ -81,6 +81,44 @@ def sample_stimulus(num_words: int = 100,
                     word_onsets, word_surprisals, p_word, candidate_phonemes)
 
 
+def response_model(stim: Stimulus,
+                   recognition_onsets: TensorType[N_W, float],
+                   params: rr.ModelParameters,
+                   num_sensors: int = 1,
+                   sample_rate: int = 128,
+                   noise_params: Tuple[float, float] = (0., 0.5),
+                   right_padding: float = 0.,
+                   ) -> torch.Tensor:
+    """
+    Generate a response time series for the given stimulus and response parameters.
+    """
+
+    # Compute recognition onset as global index
+    recognition_onsets_global_samp = time_to_sample(stim.word_onsets + recognition_onsets, sample_rate)
+
+    # Generate continuous signal stream.
+    t_max = stim.phoneme_onsets_global[-1, -1] + right_padding
+    Y = torch.normal(*noise_params,
+                     size=(int(np.ceil(t_max * sample_rate)), num_sensors))
+
+    # Sample a standardized response, which will be scaled by per-word surprisal.
+    # TODO check that window size is sufficient to cover this
+    window_std = params.b  # NB this means there's nontrivial response outside of b window.
+    window_center = params.a + window_std / 2
+    unit_response_xs, unit_response_ys = gaussian_window(window_center.item(), window_std.item(),
+                                                         sample_rate=sample_rate)
+    response_width = len(unit_response_xs)
+    unit_response_ys = torch.tensor(unit_response_ys).view(-1, 1).tile((1, num_sensors))
+
+    # Add characteristic response after each recognition onset.
+    for word_surp, rec_onset_samp in zip(stim.word_surprisals, recognition_onsets_global_samp):
+        start_idx = rec_onset_samp
+        end_idx = start_idx + response_width
+        Y[start_idx:end_idx] += params.coef[1] * word_surp * unit_response_ys
+
+    return Y
+
+
 @typechecked
 def sample_dataset(params: rr.ModelParameters,
                    num_sensors: int = 1,
@@ -100,36 +138,16 @@ def sample_dataset(params: rr.ModelParameters,
     recognition_points = rr.recognition_point_model(p_word_posterior,
                                                     stim.word_lengths,
                                                     threshold=params.threshold)
-                                                    
-    ############
-
+    
     # Compute recognition onset, relative to word onset
     recognition_onsets = torch.gather(stim.phoneme_onsets, 1,
                                       recognition_points.unsqueeze(1)).squeeze(1)
-    # Compute recognition onset as global index
-    recognition_onsets_global_samp = time_to_sample(stim.word_onsets + recognition_onsets, sample_rate)
+                                                    
+    ############
 
-    # Generate continuous signal stream.
-    t_max = stim.phoneme_onsets_global[-1, -1] + (epoch_window[1] - epoch_window[0])
-    Y = torch.normal(*noise_params,
-                     size=(int(np.ceil(t_max * sample_rate)), num_sensors))
-
-    # Sample a standardized response, which will be scaled by per-word surprisal.
-    # TODO check that window size is sufficient to cover this
-    window_std = params.b  # NB this means there's nontrivial response outside of b window.
-    window_center = params.a + window_std / 2
-    unit_response_xs, unit_response_ys = gaussian_window(window_center.item(), window_std.item(),
-                                                         sample_rate=sample_rate)
-    response_width = len(unit_response_xs)
-    unit_response_ys = torch.tensor(unit_response_ys).view(-1, 1).tile((1, num_sensors))
-
-    # Add delta response after each recognition onset.
-    # response_delay = time_to_sample(params.a, sample_rate)
-    # response_width = time_to_sample(params.b, sample_rate)
-    for word_surp, rec_onset_samp in zip(stim.word_surprisals, recognition_onsets_global_samp):
-        start_idx = rec_onset_samp
-        end_idx = start_idx + response_width
-        Y[start_idx:end_idx] += params.coef[1] * word_surp * unit_response_ys
+    epoch_width = epoch_window[1] - epoch_window[0]
+    Y = response_model(stim, recognition_onsets, params, num_sensors, sample_rate,
+                       noise_params=noise_params, right_padding=epoch_width)
 
     #############
     # Run epoching.
