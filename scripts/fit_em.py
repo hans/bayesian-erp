26b8f183ddc44a61a8c0b128df4d0b67c9fc9349
Jon Gauthier <jon@gauthiers.net>
2022-09-12 22:21:00 -0400

    fit_em cleanup + actually work sensibly with EM flow

--- scripts/fit_em.py
+++ scripts/fit_em.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from pathlib import Path
 import pickle
 
@@ -13,21 +14,22 @@ from tqdm.auto import tqdm, trange
 
 from berp.config import Config, CVConfig
 from berp.cv import OptunaSearchCV, EarlyStopException
-from berp.models.trf_em import GroupBerpTRFForwardPipeline, GroupTRFForwardPipeline
+from berp.models.trf_em import GroupTRFForwardPipeline, BerpTRFEMEstimator
 from berp.viz.trf import trf_to_dataframe, plot_trf_coefficients
+
+
+L = logging.getLogger(__name__)
 
 
 def make_cv(model, cfg: CVConfig):
     """
     Make cross-validation object.
     """
-    # TODO if we want to customize sampler, we have to set up a "study"
-    param_sampler = hydra.utils.instantiate(cfg.param_sampler)
     param_distributions = {}
     for name, dist_cfg in cfg.params.items():
         param_distributions.update(hydra.utils.call(dist_cfg, name=name))
     
-    sampler = optuna.samplers.TPESampler(multivariate=True)
+    sampler = hydra.utils.instantiate(cfg.param_sampler)
     study = optuna.create_study(sampler=sampler, direction="maximize")
     
     n_trials = cfg.n_trials if len(cfg.params) > 0 else 1
@@ -64,11 +66,12 @@ def main(cfg: Config):
     params_dir = Path("params")
     params_dir.mkdir()
 
-    # TODO support running without CV / k-fold? Not efficient. Although it does
-    # allow us to get error bounds on threshold params. If we keep the underlying
-    # fold estimators that is ..
-    if True:  # len(cfg.cv.params) > 0:
-        # Run K-fold cross validation to estimate hyperparameters.
+    # TODO outer CV
+
+    if len(cfg.cv.params) > 0:
+        L.info(f"Running cross-validation to estimate hyperparameters, with "
+               f"{cfg.cv.n_inner_folds} inner folds.")
+
         cv = make_cv(model, cfg.cv)
         cv.fit(data_train)
 
@@ -77,6 +80,27 @@ def main(cfg: Config):
 
         np.savez(params_dir / "hparams.npz", **cv.best_params_)
         est = cv.best_estimator_
+    else:
+        assert isinstance(model, BerpTRFEMEstimator), 'only supported for EM'
+
+        # TODO cv. For now, just fit on the whole train set.
+        L.warning("outer CV folds not yet implemented. Training on whole dataset")
+
+        # HACK collapse learning rates
+        # lr_cut_factor = 10  # TODO configurable
+        # L.info("Cutting encoder learning rates by factor of %d", lr_cut_factor)
+        # for enc in model.pipeline.encoders_.values():
+        #     enc.optim.set_params(learning_rate=enc.optim.learning_rate / lr_cut_factor)
+
+        # TODO is this a healthy val set?
+        data_train, data_val = train_test_split(
+            data_train, test_size=0.1, shuffle=False)
+        try:
+            model.fit(data_train, X_val=data_val, use_tqdm=True)
+        except EarlyStopException: pass
+
+        est = model
+
     # else:
     #     for _ in trange(cfg.cv.max_iter):
     #         try:
