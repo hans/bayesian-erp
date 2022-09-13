04cf5ee6977ee03cda0846ddd069118f1214d2fa
Jon Gauthier <jon@gauthiers.net>
2022-09-12 21:06:45 -0400

    load_eeg_dataseT: normalize X_variable wrt time series, not wrt itself. this fixes scale issues

--- berp/datasets/eeg.py
+++ berp/datasets/eeg.py
@@ -3,6 +3,7 @@ from typing import *
 
 from hydra.utils import to_absolute_path
 import mne
+import torch
 
 from berp.datasets.base import BerpDataset, NestedBerpDataset
 
@@ -19,15 +20,24 @@ def load_eeg_dataset(paths: List[str], montage_name: str,
 
     dataset = NestedBerpDataset(datasets)
 
-    def norm_ts(tensor):
-        return (tensor - tensor.mean(dim=0, keepdim=True)) / tensor.std(dim=0, keepdim=True)
+    def norm_ts(tensor, add_zeros=None):
+        # impute N add_zeros when computing mean and std
+        if add_zeros is not None:
+            ref_tensor = torch.cat([tensor, torch.zeros(add_zeros, *tensor.shape[1:])], dim=0)
+        else:
+            ref_tensor = tensor
+        return (tensor - ref_tensor.mean(dim=0, keepdim=True)) / ref_tensor.std(dim=0, keepdim=True)
 
     if normalize_X_ts or normalize_X_variable or normalize_Y:
         for ds in dataset.datasets:
             if normalize_X_ts:
                 ds.X_ts = norm_ts(ds.X_ts)
             if normalize_X_variable:
-                ds.X_variable = norm_ts(ds.X_variable)
+                # X_variable will be scattered onto X_ts shape, with zeros elsewhere.
+                # Account for these zeros when rescaling data, so that all predictors
+                # are on the same scale.
+                add_zeros = ds.X_ts.shape[0] - ds.X_variable.shape[0]
+                ds.X_variable = norm_ts(ds.X_variable, add_zeros=add_zeros)
             if normalize_Y:
                 ds.Y = norm_ts(ds.Y)
     
