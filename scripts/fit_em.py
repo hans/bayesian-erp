50eb90d38e974868e6fa71611410c65ba9a20738
Jon Gauthier <jon@gauthiers.net>
2022-09-17 16:35:16 -0400

    fit_em: refit and plot coefs to tensorboard whenever we get a new optimal trial

--- scripts/fit_em.py
+++ scripts/fit_em.py
@@ -12,7 +12,7 @@ from sklearn.base import clone, BaseEstimator
 from sklearn.model_selection import KFold, train_test_split
 from tqdm.auto import tqdm, trange
 
-from berp.config import Config, CVConfig
+from berp.config import Config, CVConfig, VizConfig
 from berp.cv import OptunaSearchCV, EarlyStopException
 from berp.models.trf_em import GroupTRFForwardPipeline, BerpTRFEMEstimator
 from berp.tensorboard import Tensorboard
@@ -26,106 +26,8 @@ optuna.logging.enable_propagation()
 optuna.logging.disable_default_handler()
 
 
-def tb_callback(study, trial):
+def checkpoint_model(est, dataset, params_dir, viz_cfg: VizConfig):
     tb = Tensorboard.instance()
-    tb.global_step += 1
-    if study.best_trial.number == trial.number:
-        for param, value in trial.params.items():
-            tb.add_scalar(f"optuna/{param}", value)
-        tb.add_scalar("optuna/test_score", trial.value)
-
-
-def make_cv(model, cfg: CVConfig):
-    """
-    Make cross-validation object.
-    """
-    param_distributions = {}
-    for name, dist_cfg in cfg.params.items():
-        param_distributions.update(hydra.utils.call(dist_cfg, name=name))
-    
-    sampler = hydra.utils.instantiate(cfg.param_sampler)
-    study = optuna.create_study(sampler=sampler, direction="maximize")
-    
-    n_trials = cfg.n_trials if len(cfg.params) > 0 else 1
-    return OptunaSearchCV(
-        estimator=clone(model),
-        study=study,
-        enable_pruning=False,
-        max_iter=cfg.max_iter, n_trials=n_trials,
-        param_distributions=param_distributions,
-        error_score="raise",
-        cv=KFold(n_splits=cfg.n_inner_folds, shuffle=False),
-        refit=True,
-        verbose=1,
-        callbacks=[tb_callback],)
-
-
-@hydra.main(version_base=None, config_path="../conf", config_name="config.yaml")
-def main(cfg: Config):
-    print(OmegaConf.to_yaml(cfg))
-
-    # Set up Tensorboard singleton instance before instantiating data/model classes.
-    tb = hydra.utils.call(cfg.viz.tensorboard)
-
-    dataset = hydra.utils.call(cfg.dataset)
-    dataset.set_n_splits(4)
-
-    model = hydra.utils.call(cfg.model,
-                             phonemes=dataset.phonemes,
-                             n_outputs=dataset.n_sensors,
-                             optim=cfg.solver)
-    from pprint import pprint; pprint(model.get_params())
-
-    # Before splitting datasets, prime model pipeline with full data.
-    model.prime(dataset)
-
-    data_train, data_test = train_test_split(dataset, test_size=0.25, shuffle=False)
-
-    params_dir = Path("params")
-    params_dir.mkdir()
-
-    # TODO outer CV
-
-    if len(cfg.cv.params) > 0:
-        L.info(f"Running cross-validation to estimate hyperparameters, with "
-               f"{cfg.cv.n_inner_folds} inner folds.")
-
-        cv = make_cv(model, cfg.cv)
-        cv.fit(data_train)
-
-        # Save study information for all hparam options.
-        cv.study.trials_dataframe().to_csv("trials.csv", index=False)
-
-        np.savez(params_dir / "hparams.npz", **cv.best_params_)
-        est = cv.best_estimator_
-    else:
-        assert isinstance(model, BerpTRFEMEstimator), 'only supported for EM'
-
-        # TODO cv. For now, just fit on the whole train set.
-        L.warning("outer CV folds not yet implemented. Training on whole dataset")
-
-        # HACK collapse learning rates
-        # lr_cut_factor = 10  # TODO configurable
-        # L.info("Cutting encoder learning rates by factor of %d", lr_cut_factor)
-        # for enc in model.pipeline.encoders_.values():
-        #     enc.optim.set_params(learning_rate=enc.optim.learning_rate / lr_cut_factor)
-
-        # TODO is this a healthy val set?
-        data_train, data_val = train_test_split(
-            data_train, test_size=0.1, shuffle=False)
-        try:
-            model.fit(data_train, X_val=data_val, use_tqdm=True)
-        except EarlyStopException: pass
-
-        est = model
-
-    # else:
-    #     for _ in trange(cfg.cv.max_iter):
-    #         try:
-    #             model.partial_fit(data_train)
-    #         except EarlyStopException:
-    #             break
-    #     est = model
 
     # May have a wrapper around it.
     if hasattr(est, "pipeline"):
@@ -164,9 +66,136 @@ def main(cfg: Config):
         fig = plot_trf_coefficients(
             encoder,
             feature_names=feature_names,
-            feature_match_patterns=cfg.viz.feature_patterns)
+            feature_match_patterns=viz_cfg.feature_patterns)
         fig.savefig(params_dir / f"encoder_coefs.{key}.png")
         tb.add_figure(f"encoder_coefs/{key}", fig)
+
+
+def make_tb_callback(est, dataset, params_dir, viz_cfg: VizConfig):
+    def tb_callback(study, trial):
+        tb = Tensorboard.instance()
+        tb.global_step += 1
+        if study.best_trial.number == trial.number:
+            for param, value in trial.params.items():
+                tb.add_scalar(f"optuna/{param}", value)
+            tb.add_scalar("optuna/test_score", trial.value)
+
+            # Refit and checkpoint model.
+            L.info("Refitting and checkpointing model")
+            est_i = clone(est)
+            est_i.set_params(**trial.params)
+            est_i.fit(dataset)
+            checkpoint_model(est_i, dataset, params_dir, viz_cfg)
+    
+    return tb_callback
+
+
+def make_cv(model, cfg: CVConfig, callbacks=None):
+    """
+    Make cross-validation object.
+    """
+    if callbacks is None:
+        callbacks = []
+
+    param_distributions = {}
+    for name, dist_cfg in cfg.params.items():
+        param_distributions.update(hydra.utils.call(dist_cfg, name=name))
+    
+    sampler = hydra.utils.instantiate(cfg.param_sampler)
+    study = optuna.create_study(sampler=sampler, direction="maximize")
+    
+    n_trials = cfg.n_trials if len(cfg.params) > 0 else 1
+    return OptunaSearchCV(
+        estimator=clone(model),
+        study=study,
+        enable_pruning=False,
+        max_iter=cfg.max_iter, n_trials=n_trials,
+        param_distributions=param_distributions,
+        error_score="raise",
+        cv=KFold(n_splits=cfg.n_inner_folds, shuffle=False),
+        refit=True,
+        verbose=1,
+        callbacks=callbacks,)
+
+
+@hydra.main(version_base=None, config_path="../conf", config_name="config.yaml")
+def main(cfg: Config):
+    print(OmegaConf.to_yaml(cfg))
+
+    # Set up Tensorboard singleton instance before instantiating data/model classes.
+    tb = hydra.utils.call(cfg.viz.tensorboard)
+
+    dataset = hydra.utils.call(cfg.dataset)
+    dataset.set_n_splits(4)
+
+    model = hydra.utils.call(cfg.model,
+                             phonemes=dataset.phonemes,
+                             n_outputs=dataset.n_sensors,
+                             optim=cfg.solver)
+    from pprint import pprint; pprint(model.get_params())
+
+    # Before splitting datasets, prime model pipeline with full data.
+    model.prime(dataset)
+
+    # DEV: use a much smaller training set for dev cycle efficiency
+    # test_size = 0.75
+    # L.warning("Using a teeny training set for dev purposes")
+    test_size = .25
+    data_train, data_test = train_test_split(dataset, test_size=test_size, shuffle=False)
+
+    params_dir = Path("params")
+    params_dir.mkdir()
+
+    # TODO outer CV
+
+    if len(cfg.cv.params) > 0:
+        L.info(f"Running cross-validation to estimate hyperparameters, with "
+               f"{cfg.cv.n_inner_folds} inner folds.")
+
+        tb_callback = make_tb_callback(model, data_train, params_dir, cfg.viz)
+        cv = make_cv(model, cfg.cv, callbacks=[tb_callback])
+        cv.fit(data_train)
+
+        # Save study information for all hparam options.
+        cv.study.trials_dataframe().to_csv("trials.csv", index=False)
+
+        np.savez(params_dir / "hparams.npz", **cv.best_params_)
+        est = cv.best_estimator_
+    else:
+        assert isinstance(model, BerpTRFEMEstimator), 'only supported for EM'
+
+        # TODO cv. For now, just fit on the whole train set.
+        L.warning("outer CV folds not yet implemented. Training on whole dataset")
+
+        # HACK collapse learning rates
+        lr_cut_factor = 100  # TODO configurable
+        alpha_reset = None
+        L.info("Cutting encoder learning rates by factor of %d", lr_cut_factor)
+        if alpha_reset is not None:
+            L.info("Setting alpha to %d", alpha_reset)
+        for enc in model.pipeline.encoders_.values():
+            enc.optim.set_params(learning_rate=enc.optim.learning_rate / lr_cut_factor)
+            if alpha_reset is not None:
+                enc.set_params(alpha=alpha_reset)
+
+        # TODO is this a healthy val set?
+        data_train, data_val = train_test_split(
+            data_train, test_size=0.1, shuffle=False)
+        try:
+            model.fit(data_train, X_val=data_val, use_tqdm=True)
+        except EarlyStopException: pass
+
+        est = model
+
+    # else:
+    #     for _ in trange(cfg.cv.max_iter):
+    #         try:
+    #             model.partial_fit(data_train)
+    #         except EarlyStopException:
+    #             break
+    #     est = model
+
+    checkpoint_model(est, dataset, params_dir, cfg.viz)
 
     tb.close()
 
