ed25a1b1935ec044a11b858a26f1223a4c444198
Jon Gauthier <jon@gauthiers.net>
2022-11-09 17:47:04 -0500

    add BaselinedScorer

---
+++ test/cv/test_evaluation.py
@@ -0,0 +1,37 @@
+from unittest.mock import MagicMock
+
+import numpy as np
+
+from berp.cv.evaluation import BaselinedScorer
+from berp.datasets import NestedBerpDataset
+from berp.models.trf_em import GroupTRFForwardPipeline
+
+
+class TestBaselinedScorer:
+
+    def test_baseline_numeric(self):
+        n_sensors = 10
+        baseline_scores = np.random.random((3, n_sensors))
+        scores = np.random.random((3, n_sensors))
+
+        baseline_model = MagicMock(
+            spec=GroupTRFForwardPipeline,
+            score_multidimensional=lambda X, y: baseline_scores,
+        )
+
+        estimator = MagicMock(
+            spec=GroupTRFForwardPipeline,
+            score_multidimensional=lambda X, y: scores,
+        )
+
+        X = MagicMock(spec=NestedBerpDataset, n_sensors=n_sensors)
+
+        scorer = BaselinedScorer(baseline_model)
+        ret = scorer(estimator, X)
+        assert isinstance(ret, np.floating)
+        assert ret == np.mean(scores - baseline_scores)
+
+        scorer = BaselinedScorer(baseline_model, aggregation_fn=lambda x: x)
+        ret = scorer(estimator, X)
+        assert isinstance(ret, np.ndarray)
+        np.testing.assert_allclose(ret, scores - baseline_scores)
