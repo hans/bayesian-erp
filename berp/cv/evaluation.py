fe0177c6d6378be6d36f5e1f2c8f8e27a96d2ed3
Jon Gauthier <jon@gauthiers.net>
2022-11-15 20:02:17 -0500

    cv.evaluation: dump all scores on Tensorboard

--- berp/cv/evaluation.py
+++ berp/cv/evaluation.py
@@ -1,12 +1,19 @@
 
-from typing import Callable, TypeVar, Generic, Optional
+from typing import Callable, TypeVar, Generic, Optional, Union
 from typeguard import typechecked
 
 import numpy as np
 
 from berp.datasets import BerpDataset, NestedBerpDataset
 from berp.models.trf_em import GroupTRFForwardPipeline
+from berp.tensorboard import tb_add_scalar, tb_global_step
 
+
+def log_scores(dataset: NestedBerpDataset, scores: np.ndarray, tag="score"):
+    for ds, ds_scores in zip(dataset.datasets, scores):
+        assert ds.sensor_names is not None
+        for sensor_idx, sensor_score in enumerate(ds_scores):
+            tb_add_scalar(f"{tag}/{ds.name}/{ds.sensor_names[sensor_idx]}", sensor_score)
 
 Score = TypeVar("Score")
 class BaselinedScorer(Generic[Score]):
@@ -23,9 +30,13 @@ class BaselinedScorer(Generic[Score]):
         self.aggregation_fn = aggregation_fn
 
     def __call__(self, estimator: GroupTRFForwardPipeline,
-                 X, y=None) -> Score:
+                 X: Union[BerpDataset, NestedBerpDataset],
+                 y=None) -> Score:
+        if isinstance(X, BerpDataset):
+            X = NestedBerpDataset([X])
         
         score = estimator.score_multidimensional(X, y)
+        log_scores(X, score, tag="score/est")
 
         if self.baseline_model is not None:
             baseline_score = self.baseline_model.score_multidimensional(X, y)
@@ -38,6 +49,10 @@ class BaselinedScorer(Generic[Score]):
                 raise ValueError()
             assert baseline_score.shape == score.shape
 
-            score = score - baseline_score
+            log_scores(X, baseline_score, tag="score/baseline")
 
+            score = score - baseline_score
+            log_scores(X, score, tag="score/delta")
+
+        tb_global_step()
         return self.aggregation_fn(score)
