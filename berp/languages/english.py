7626826d0d9a3c6ec26bc98ed8a2a461be3834fa
Jon Gauthier <jon@gauthiers.net>
2023-05-10 16:35:51 -0400

    add syllabification logic to run_language_model; bottom out syllable level features in phoneme features

--- berp/languages/english.py
+++ berp/languages/english.py
@@ -234,3 +234,152 @@ class Phonemizer:
 
             # HACK
             return tuple(self.ipa_phonemes_re.findall(string))
+
+
+from nltk.tokenize.api import TokenizerI
+from nltk.util import ngrams
+from string import punctuation
+import warnings
+
+class IPASyllableTokenizer(TokenizerI):
+    # Based on NLTK SyllableTokenizer
+
+    english_ipa_sonority_hierarchy = [
+        ['aɪ', 'aʊ', 'i', 'oʊ', 'u', 'æ', 'ɑ', 'ɔ', 'ɔɪ', 'ɚ', 'ɛ', 'ɛɪ', 'ɪ', 'ʊ', 'ʌ',], # vowels
+        ['j', 'l', 'm', 'n', 'w', 'ŋ', 'ɹ'], # nasals and approximants
+        ['f', 'h', 's', 'v', 'z', 'ð', 'ʃ', 'ʒ', 'θ'], # fricatives
+        ['b', 'd', 'dʒ', 'g', 'k', 'p', 't', 'tʃ'], # stops and affricates
+    ]
+
+    def __init__(self, sonority_hierarchy=None):
+        """
+        :param lang: Language parameter, default is English, 'en'
+        :type lang: str
+        :param sonority_hierarchy: Sonority hierarchy according to the
+                                   Sonority Sequencing Principle.
+        :type sonority_hierarchy: list(str)
+        """
+        sonority_hierarchy = sonority_hierarchy or self.english_ipa_sonority_hierarchy
+
+        self.vowels = sonority_hierarchy[0]
+        self.phoneme_map = {}
+        for i, level in enumerate(sonority_hierarchy):
+            for c in level:
+                sonority_level = len(sonority_hierarchy) - i
+                self.phoneme_map[c] = sonority_level
+                self.phoneme_map[c.upper()] = sonority_level
+
+
+    def assign_values(self, token):
+        """
+        Assigns each phoneme its value from the sonority hierarchy.
+        Note: Sentence/text has to be tokenized first.
+
+        :param token: Single word or token
+        :type token: str
+        :return: List of tuples, first element is character/phoneme and
+                 second is the soronity value.
+        :rtype: list(tuple(str, int))
+        """
+        syllables_values = []
+        for c in token:
+            try:
+                syllables_values.append((c, self.phoneme_map[c]))
+            except KeyError:
+                if c not in "0123456789" and c not in punctuation:
+                    warnings.warn(
+                        "Character not defined in sonority_hierarchy,"
+                        " assigning as vowel: '{}'".format(c)
+                    )
+                    syllables_values.append((c, max(self.phoneme_map.values())))
+                    if c not in self.vowels:
+                        self.vowels += c
+                else:  # If it's a punctuation or numbers, assign -1.
+                    syllables_values.append((c, -1))
+        return syllables_values
+
+
+    def validate_syllables(self, syllable_list):
+        """
+        Ensures each syllable has at least one vowel.
+        If the following syllable doesn't have vowel, add it to the current one.
+
+        :param syllable_list: Single word or token broken up into syllables.
+        :type syllable_list: list(str)
+        :return: Single word or token broken up into syllables
+                 (with added syllables if necessary)
+        :rtype: list(str)
+        """
+        valid_syllables = []
+        front = []
+        vowel_pattern = re.compile("|".join(self.vowels))
+        for i, syllable in enumerate(syllable_list):
+            syllable_str = "".join(syllable)
+            if syllable_str in punctuation:
+                valid_syllables.append(syllable)
+                continue
+            if not vowel_pattern.search(syllable_str):
+                if len(valid_syllables) == 0:
+                    front += syllable
+                else:
+                    valid_syllables = valid_syllables[:-1] + [
+                        valid_syllables[-1] + syllable
+                    ]
+            else:
+                if len(valid_syllables) == 0:
+                    valid_syllables.append(front + syllable)
+                else:
+                    valid_syllables.append(syllable)
+
+        return valid_syllables
+
+
+    def tokenize(self, token):
+        """
+        Apply the SSP to return a list of syllables.
+        Note: Sentence/text has to be tokenized first.
+
+        :param token: Single word or token
+        :type token: str
+        :return syllable_list: Single word or token broken up into syllables.
+        :rtype: list(str)
+        """
+        # assign values from hierarchy
+        syllables_values = self.assign_values(token)
+
+        # if only one vowel return word
+        if sum(token.count(x) for x in self.vowels) <= 1:
+            return [token]
+
+        syllable_list = []
+        syllable = [syllables_values[0][0]]  # start syllable with first phoneme
+        for trigram in ngrams(syllables_values, n=3):
+            phonemes, values = zip(*trigram)
+            # Sonority of previous, focal and following phoneme
+            prev_value, focal_value, next_value = values
+            # Focal phoneme.
+            focal_phoneme = phonemes[1]
+
+            # These cases trigger syllable break.
+            if focal_value == -1:  # If it's a punctuation, just break.
+                syllable_list.append(syllable)
+                syllable_list.append(focal_phoneme)
+                syllable = []
+            elif prev_value >= focal_value == next_value:
+                syllable.append(focal_phoneme)
+                syllable_list.append(syllable)
+                syllable = []
+
+            elif prev_value > focal_value < next_value:
+                syllable_list.append(syllable)
+                syllable = []
+                syllable.append(focal_phoneme)
+
+            # no syllable break
+            else:
+                syllable.append(focal_phoneme)
+
+        syllable.append(syllables_values[-1][0])  # append last phoneme
+        syllable_list.append(syllable)
+
+        return self.validate_syllables(syllable_list)
